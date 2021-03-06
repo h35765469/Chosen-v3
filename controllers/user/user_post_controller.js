6f891220b1e1ad90522b8629612ea67851d7c373
h35765459 <h35765469@gmail.com>
2021-03-06 10:06:33 +0800

    user post controller

---
+++ controllers/user/user_post_controller.js
@@ -0,0 +1,43 @@
+const Check = require('../../service/member_check');
+
+const verify = require('../../models/member/verification_model');
+const UserModel = require('../../models/member/user_model');
+const ReturnCodeConfig = require('../../service/ReturnCodeConfig');
+const VerificationModel = require('../../models/member/verification_model');
+
+var check = new Check();
+
+module.exports = class PostUser 
+{
+    //用真錢購買虛擬貨幣
+    postBuyVirtualMoney(req, res, next)
+    {
+        const token = req.headers['token'];
+        //確定token是否有輸入
+        if(check.checkNull(token) === true)
+        {
+            res.json(ReturnCodeConfig.response(400, "請輸入token！", "", {}))
+        }
+        else 
+        {
+            VerificationModel.verifyTokenInDataBase(token).then(tokenResult => {
+                if(tokenResult === false)
+                {
+                    res.json(ReturnCodeConfig.response(400, "token失誤", "", {}))
+                }
+                else
+                {
+                    UserModel.getUserBuyProductData(tokenResult[0].id).then(result =>
+                        {
+                            res.json(ReturnCodeConfig.response('0000', 'success', '', result))
+                        }, (err) =>
+                        {
+                            res.json({
+                                result: err
+                            })
+                        })
+                }
+            });
+        }
+    }
+}
