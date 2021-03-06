0084ec9b74093b6401d998fa76bc02d7fa4fa2ca
h35765459 <h35765469@gmail.com>
2021-03-06 10:05:10 +0800

    add getReToken() and getRandomId() function

--- models/member/encryption.js
+++ models/member/encryption.js
@@ -8,3 +8,20 @@ module.exports = function getRePassword(password) {
     //   console.log('rePassword: ' + rePassword);
     return rePassword;
 }
+
+//產出token
+module.exports = function getReToken(token)
+{
+    //加密
+    let hashToken = crypto.createHash('sha2');
+    hashToken.update(token);
+    const reToken = hashToken.digest('hex');
+    //   console.log('rePassword: ' + rePassword);
+    return reToken;
+}
+
+//產出隨機id
+module.exports = function getRandomId()
+{
+    return crypto.randomBytes(16).toString("hex");;
+}
