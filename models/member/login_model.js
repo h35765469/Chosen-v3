cba13291bb784d52d47ba74ce9d11f56388182bb
h35765459 <h35765469@gmail.com>
2021-03-07 20:53:52 +0800

    change console.log("gg " + result); to console.log("gg " + result.userToken);

--- models/member/login_model.js
+++ models/member/login_model.js
@@ -184,7 +184,7 @@ let goFbLogin = function(resolve, reject, loginType, loginToken)
                         money: rows[0].money,
                         userToken:rows[0].token,
                     };
-                    console.log("gg " + result);
+                    console.log("gg " + result.userToken);
                     resolve(ReturnCodeConfig.response('0000', '登入成功', '', result));
                 } else {
                     generateUser(resolve, fbRes);
