f792efb22322bb52aafb10390b0dc2ffb8eafc98
h35765459 <h35765469@gmail.com>
2021-03-07 23:49:51 +0800

    delete console.log("fuck " + response);

--- controllers/member/modify_controller.js
+++ controllers/member/modify_controller.js
@@ -127,7 +127,6 @@ module.exports = class Member {
                 res.json(ReturnCodeConfig.response('404', '登入失敗。', '', {}));
             } else if (check.checkNull(response) === false) {
 
-                console.log("fuck " + response);
                 //res.setHeader('token', rows[0].token);
                 res.json(
                     response
