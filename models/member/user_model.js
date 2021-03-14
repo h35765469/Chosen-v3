34a9ad5515f8483173662ab83762f991b3d2fcf1
h35765459 <h35765469@gmail.com>
2021-03-14 15:53:00 +0800

    use ReturnCodeConfig.response()

--- models/member/user_model.js
+++ models/member/user_model.js
@@ -2,15 +2,19 @@
 
 const { resolve } = require('bluebird');
 const db = require('../connection_db');
+const ReturnCodeConfig = require('../../service/ReturnCodeConfig')
+
 module.exports.getUserBuyProductData = function (userId) {
     return new Promise((resolve, reject) => {
         //獲取玩家所有購買造型。
         db.query('SELECT product.id, product.product_category_id, product.name, product.preview_img_url, product.description FROM product INNER JOIN user_product_list ON product.id = user_product_list.product_id WHERE user_product_list.user_id = ?', userId, function (err, rows) {
             if (err) {
-                reject(err);
+                reject(ReturnCodeConfig.response('504', '獲取資料失敗', 'none', err));
                 return;
             }
-            resolve(rows);
+            var result = {};
+            result.user_products = rows;
+            resolve(ReturnCodeConfig.response('0000', '獲取資料成功', 'none', result));
         });
     });
 }
