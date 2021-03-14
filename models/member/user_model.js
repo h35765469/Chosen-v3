30d0f9b5b9e4e1193ca7be41997c97f5e8f9b2bf
h35765459 <h35765469@gmail.com>
2021-03-14 16:07:37 +0800

    console.log("pounce " + userId);

--- models/member/user_model.js
+++ models/member/user_model.js
@@ -5,6 +5,7 @@ const db = require('../connection_db');
 const ReturnCodeConfig = require('../../service/ReturnCodeConfig')
 
 module.exports.getUserBuyProductData = function (userId) {
+    console.log("pounce " + userId);
     return new Promise((resolve, reject) => {
         //獲取玩家所有購買造型。
         db.query('SELECT product.id, product.product_category_id, product.name, product.preview_img_url, product.description FROM product INNER JOIN user_product_list ON product.id = user_product_list.product_id WHERE user_product_list.user_id = ?', userId, function (err, rows) {
