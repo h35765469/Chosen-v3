f075bef88c01166946ecd4904ed60b251c767eb7
h35765459 <h35765469@gmail.com>
2021-03-14 11:43:39 +0800

    use ReturnCodeConfig.response()

--- models/product/getAllProduct_model.js
+++ models/product/getAllProduct_model.js
@@ -32,13 +32,11 @@ module.exports.getShopProductData = function()
             // 若資料庫部分出現問題，則回傳「伺服器錯誤，請稍後再試！」的結果。
             if (err) {
                 console.log(err);
-                result.status = "取得全部訂單資料失敗。"
-                result.err = "伺服器錯誤，請稍後在試！"
-                reject(result);
+                reject(ReturnCodeConfig.response('504', '獲取資料失敗', 'none', err));
                 return;
             }
             // 若資料庫部分沒問題，則回傳全部產品資料。
-            resolve(rows);
+            resolve(ReturnCodeConfig.response('504', '獲取資料失敗', 'none', rows));
         });
     });
 }
