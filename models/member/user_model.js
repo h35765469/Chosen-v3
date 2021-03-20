9aeb38ed0c5e66b68989979580b690c77b6b1062
h35765459 <h35765469@gmail.com>
2021-03-20 12:15:09 +0800

    delete console.log("mite " + result);

--- models/member/user_model.js
+++ models/member/user_model.js
@@ -26,7 +26,6 @@ module.exports.getUser = function(userId)
                     return;
                 }
                 result.selected_locker_products = rows;
-                console.log("mite " + result);
                 resolve(ReturnCodeConfig.response('0000', '獲取資料成功', 'none', result));
             });
         });
