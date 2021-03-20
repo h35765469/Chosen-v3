ca2af140dbef147550a55f553b37a21dfabb8708
h35765459 <h35765469@gmail.com>
2021-03-20 11:53:19 +0800

    add database query 獲取玩家所選造型。

--- models/member/user_model.js
+++ models/member/user_model.js
@@ -16,6 +16,18 @@ module.exports.getUser = function(userId)
             var result = {};
             result.nickname = rows[0].nickname;
             result.money = rows[0].money;
+
+            //獲取玩家所選造型。
+            db.query("SELECT product.id, product.product_category_id, product.name, product.preview_img_url, product.description FROM product" +
+            "INNER JOIN user_select_product ON product.id = user_select_product.product_id" + 
+            "WHERE user_select_product.user_id = ?", userId, function (err, rows) {
+                if (err) {
+                    reject(ReturnCodeConfig.response('504', '獲取資料失敗', 'none', err));
+                    return;
+                }
+                result.selected_locker_products = rows;
+                resolve(ReturnCodeConfig.response('0000', '獲取資料成功', 'none', result));
+            });
             resolve(ReturnCodeConfig.response('0000', '獲取資料成功', 'none', result));
         });
     });
