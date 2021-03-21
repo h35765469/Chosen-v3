63765809cb15a425386ba71201e15155ce99aba6
h35765459 <h35765469@gmail.com>
2021-03-21 22:11:49 +0800

    add //更換造型 router.post('/update_selected_locker_product', userPostController.postSelectedLockerProduct)

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 const UserGetController = require('../controllers/user/user_get_controller');
+const UserPostController = require('../controllers/user/user_post_controller');
 
 const userGetController = new UserGetController();
+const userPostController = new UserPostController();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,5 +15,7 @@ router.get('/', function(req, res, next) {
 router.get('/user_data', userGetController.getUser)
 
 router.get('/user_buy_product', userGetController.getUserBuyProduct)
+
+router.post('/update_selected_locker_product', userPostController.postSelectedLockerProduct)
 
 module.exports = router;
