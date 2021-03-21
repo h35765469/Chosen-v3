c787d1e399ace6bd6251c86a26e52951efa5c157
h35765459 <h35765469@gmail.com>
2021-03-21 13:01:04 +0800

    check user token when call getShopProduct() //獲得所有商店販售商品

--- controllers/product/get_controller.js
+++ controllers/product/get_controller.js
@@ -1,5 +1,6 @@
 var ProductData = require('../../models/product/getAllProduct_model');
 var ReturnCodeConfig = require('../../service/ReturnCodeConfig')
+const verify = require('../../models/member/verification_model');
 
 module.exports = class GetProduct {
     // 取得全部產品資料
@@ -19,11 +20,24 @@ module.exports = class GetProduct {
     getShopProduct(req, res, next)
     {
       const token = req.headers['token'];
-      ProductData.getShopProductData().then(result => {
-          res.json(result)
-        }, (err) => 
-        {
-          res.json(result)
-        })
+        //確定token是否有輸入
+        if (check.checkNull(token) === true) {
+            res.json({
+                err: "請輸入token！"
+            })
+        } else if (check.checkNull(token) === false) {
+            verify.verifyTokenInDataBase(token).then(tokenResult => {
+                if (tokenResult === false) {
+                    res.json(ReturnCodeConfig.response(504, 'token 錯誤', '', {}))
+                } else {
+                  ProductData.getShopProductData().then(result => {
+                    res.json(result)
+                  }, (err) => 
+                  {
+                    res.json(err)
+                  })
+                }
+            })
+        }
     }
 }
