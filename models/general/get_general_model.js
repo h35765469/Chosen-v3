c27d1b6aefbf017f4a4f93dcf18ef91de8955b69
h35765459 <h35765469@gmail.com>
2021-03-27 14:32:06 +0800

    add resolve(ReturnCodeConfig.response('0000', '獲取資料成功', 'none', {'version': "1.0.0", 'maintain': false}));

--- models/general/get_general_model.js
+++ models/general/get_general_model.js
@@ -3,10 +3,7 @@ const db = require('../connection_db');
 const ReturnCodeConfig = require('../../service/ReturnCodeConfig')
 
 module.exports.getGeneralData = function () {
-    let result = {};
     return new Promise((resolve, reject) => {
-        result.version = "1.0.0";
-        result.maintain = false;
-        resolve(ReturnCodeConfig.response('0000', '獲取資料成功', 'none', result));
+        resolve(ReturnCodeConfig.response('0000', '獲取資料成功', 'none', {'version': "1.0.0", 'maintain': false}));
     })
 }
