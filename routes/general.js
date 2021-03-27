9fdb3a83a67bbbd88a52c2b6050ffab446e894c3
h35765459 <h35765469@gmail.com>
2021-03-27 14:14:57 +0800

    router.get('/geneal', to router.get('/general', GeneralGetController.getGeneral)

--- routes/general.js
+++ routes/general.js
@@ -4,6 +4,6 @@ var router = express.Router();
 const GeneralGetController = require('../controllers/general/general_get_controller');
 
 //獲得所有商店的商品
-router.get('/geneal', GeneralGetController.getGeneral)
+router.get('/general', GeneralGetController.getGeneral)
 
 module.exports = router;
